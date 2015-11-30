93e769988993c25987fc5084b7d68285e3ecce5c
Benedikt Rötsch <info@benedikt-roetsch.de>
2015-11-30 20:44:45 +0100

    inline renderer has now full test coverage

--- tests/src/Kernel/SvgInlineImageRendererKernelTest.php
+++ tests/src/Kernel/SvgInlineImageRendererKernelTest.php
@@ -43,15 +43,79 @@ class SvgInlineImageRendererKernelTest extends KernelTestBase {
   public function testGenerate() {
     $svg_inline_image = new SvgInlineImageRenderer();
 
+    // Read svg file and get its content for comparison.
     $svg = file_get_contents(DRUPAL_ROOT . '/' . drupal_get_path('module', 'svg_test') . '/assets/icons/drupal-8.svg');
     $crawler = new Crawler();
     $crawler->addXmlContent($svg);
+    $view_box = $crawler->attr('viewBox');
 
-    $expected_result = '<svg viewBox="' . $crawler->attr('viewBox') . '">' . $crawler->html() .  '</svg>';
-
+    // Test plain inline svg rendering.
+    $expected_result = '<svg viewBox="' . $view_box . '">' . $crawler->html() .  '</svg>';
     $result = $svg_inline_image->generate('@svg_test/assets/icons/drupal-8.svg');
-
     $this->assertEquals($expected_result, $result, 'inline svg rendering');
+
+    // Test inline svg rendering with custom viewBox.
+    $expected_result = '<svg viewBox="1 2 3 4">' . $crawler->html() .  '</svg>';
+    $result = $svg_inline_image->generate(
+      '@svg_test/assets/icons/drupal-8.svg',
+      [
+        'viewBox' => [
+          'x' => 1,
+          'y' => 2,
+          'width' => 3,
+          'height' => 4
+        ]
+      ]
+    );
+    $this->assertEquals($expected_result, $result, 'inline svg rendering with custom viewBox');
+
+    // Test inline svg rendering with custom attributes.
+    $expected_result = '<svg viewBox="' . $view_box . '" class="test-class" data-test="test-value">' . $crawler->html() .  '</svg>';
+    $result = $svg_inline_image->generate(
+      '@svg_test/assets/icons/drupal-8.svg',
+      [
+        'attributes' => [
+          'class' => 'test-class',
+          'data-test' => 'test-value'
+        ]
+      ]
+    );
+    $this->assertEquals($expected_result, $result, 'inline svg rendering with custom attributes');
+
+    // Test inline svg rendering with adjusted viewBox.
+    $view_box_array = explode(' ', $view_box);
+    $view_box_array[2] += 50;
+    $view_box_array[3] += 100;
+
+    $expected_result = '<svg viewBox="' . implode(' ', $view_box_array) . '">' . $crawler->html() .  '</svg>';
+    $result = $svg_inline_image->generate(
+      '@svg_test/assets/icons/drupal-8.svg',
+      [
+        'adjustWidth' => 50,
+        'adjustHeight' => 100,
+      ]
+    );
+    $this->assertEquals($expected_result, $result, 'inline svg rendering with adjusted viewBox');
+
+    // Combined test of all techniques to ensure the combination is working.
+    $expected_result = '<svg viewBox="1 2 53 104" data-test="test-value">' . $crawler->html() .  '</svg>';
+    $result = $svg_inline_image->generate(
+      '@svg_test/assets/icons/drupal-8.svg',
+      [
+        'adjustWidth' => 50,
+        'adjustHeight' => 100,
+        'attributes' => [
+          'data-test' => 'test-value',
+        ],
+        'viewBox' => [
+          'x' => 1,
+          'y' => 2,
+          'width' => 3,
+          'height' => 4
+        ]
+      ]
+    );
+    $this->assertEquals($expected_result, $result, 'inline svg rendering with all manipulation techniques');
   }
 
 }
